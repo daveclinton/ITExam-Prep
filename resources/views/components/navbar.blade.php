12c5af8f9c7fa69e0becb95de808ec4718a063bd
daveclinton <clintondavid46@gmail.com>
2025-09-16 14:39:22 +0300

    feat: add navbar section

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,8 +3,7 @@
         <div class="flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5">
                 <span class="sr-only">IT Exams Revision</span>
-                <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Logo"
-                    class="h-8 w-auto" />
+                <img src="{{ asset('logo.png') }}" alt="Logo" class="h-20 w-auto" />
             </a>
         </div>
         <div class="flex lg:hidden">
@@ -103,8 +102,7 @@
                     <div class="flex items-center justify-between">
                         <a href="#" class="-m-1.5 p-1.5">
                             <span class="sr-only">IT Exams Revision</span>
-                            <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                                alt="Logo" class="h-8 w-auto" />
+                            <img src="{{ asset('logo.png') }}" alt="Logo" class="h-8 w-auto" />
                         </a>
                         <button type="button" command="close" commandfor="mobile-menu"
                             class="-m-2.5 rounded-md p-2.5 text-gray-600">
